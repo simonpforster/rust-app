c263befdde0016e45c6af2486409b4813b9a47d8
Simon <simon.forster98@gmail.com>
2024-03-01 10:46:38 +0000

    cargo fix

--- loadtest/src/main.rs
+++ loadtest/src/main.rs
@@ -1,12 +1,9 @@
 use goose::prelude::*;
 use goose_eggs::{validate_and_load_static_assets, Validate};
 
-
 async fn loadtest_list_users(user: &mut GooseUser) -> TransactionResult {
     let goose_users = user.get("/").await?;
-    let validate = &Validate::builder()
-        .status(200)
-        .build();
+    let validate = &Validate::builder().status(200).build();
 
     validate_and_load_static_assets(user, goose_users, &validate).await?;
 
@@ -19,7 +16,7 @@ async fn main() -> Result<(), GooseError> {
         .register_scenario(
             scenario!("Basic endpoint")
                 .set_weight(10)?
-                .register_transaction(transaction!(loadtest_list_users))
+                .register_transaction(transaction!(loadtest_list_users)),
         )
         .set_default(GooseDefault::Host, "http://localhost:8080")?
         .set_default(GooseDefault::NoPrintMetrics, true)?
