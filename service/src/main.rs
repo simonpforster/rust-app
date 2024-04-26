1015dcba51c37a2b840dac4ca4f760b720a5534c
simonpforster <simon.forster98@gmail.com>
2024-04-26 07:33:18 +0100

    clean up some unused imports

--- service/src/main.rs
+++ service/src/main.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 use log4rs::append::console::{ConsoleAppender, Target};
 use log4rs::{Config, Handle};
 use tokio::net::TcpListener;
-use log::{info, Level, LevelFilter, logger, SetLoggerError};
+use log::{info, LevelFilter, SetLoggerError};
 use log4rs::config::{Appender, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
 use service::config::application_config::{ApplicationConfig, LoggerConfig};
