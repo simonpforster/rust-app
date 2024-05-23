4c7026383759d7a32c61393a9850a6c835576e4c
simonpforster <simon.forster98@gmail.com>
2024-05-23 14:04:26 +0100

    remove default endpoint

--- service/src/routes/default.rs
+++
@@ -1,9 +0,0 @@
-use crate::router::ResponseResult;
-use crate::routes::utils::full;
-use hyper::Response;
-use log::info;
-
-pub(crate) fn route(name: &str) -> ResponseResult {
-    info!("Hello reached");
-    Ok(Response::new(full(format!("Hello from server {}", name))))
-}
--- service/src/routes/mod.rs
+++ service/src/routes/mod.rs
@@ -1,3 +1,2 @@
-pub mod default;
 pub mod private;
 pub(crate) mod utils;
